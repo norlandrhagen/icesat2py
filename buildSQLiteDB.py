5cc42c02d7955b0c6c59bf0c173692cc6c3ce9aa
raphael <norlandrhagen@gmail.com>
2019-07-05 18:29:30 -0700

    dataset successfull inserted into testdb

--- buildSQLiteDB.py
+++ buildSQLiteDB.py
@@ -14,12 +14,12 @@ for every (hardcoded?) data product. ie laser returns, gridded data, etc... writ
 
 
 
-import pandas
+import pandas as pd
 import h5py
 import sqlite3
 import glob
 
-inputdir = '/home/nrhagen/Documents/icesat2/Ouputs/'
+inputdir = '/home/nrhagen/Documents/icesat2py/Outputs/'
 hdf5_file_list = glob.glob(inputdir + '*.h5*')
 
 def read_hdf5_headers():
@@ -27,57 +27,131 @@ def read_hdf5_headers():
     l1_keys = f.keys()
     return l1_keys
 
+
+
 l1_keys = read_hdf5_headers()
 
 sqlite_db = 'icesat2_test.sqlite'    # name of the sqlite database file
-table_name = 'gtl1_test'
+table_name = 'ATL06_test'
+
+
+
+
 def build_sqliteDB(sqlite_db, table_name):
     conn = sqlite3.connect(sqlite_db)
     c = conn.cursor()
 
-
-    c.execute("""CREATE TABLE {tn} ({gtl1_dt} {gtl1_dt_ft},
+    sqlite_str = """CREATE TABLE IF NOT EXISTS {tn} (
+    {gtl1_dt} {gtl1_dt_ft},
     {gtl1_lat} {gtl1_lat_ft},
     {gtl1_lon} {gtl1_lon_ft},
     {gtl1_h_li} {gtl1_h_li_ft},
     {gtl1_x_atc} {gtl1_x_atc_ft},
-    {gtl1_atl_06_qual} {gtl1_atl_06_qual_ft}""".format(tn=table_name,
+    {gtl1_atl_06_qual} {gtl1_atl_06_qual_ft} );""".format(tn=table_name,
     gtl1_dt='gtl1_delta_time', gtl1_dt_ft='REAL',
     gtl1_lat='gtl1_lat', gtl1_lat_ft='REAL',
     gtl1_lon='gtl1_lon', gtl1_lon_ft='REAL',
     gtl1_h_li='gtl1_h_li', gtl1_h_li_ft='REAL',
     gtl1_x_atc='gtl1_x_atc', gtl1_x_atc_ft='REAL',
-    gtl1_atl_06_qual='gtl1_atl_06_qual', gtl1_atl_06_qual_ft='INT'))
+    gtl1_atl_06_qual='gtl1_atl_06_qual', gtl1_atl_06_qual_ft='INT')
+
+    c.execute(sqlite_str)
+
 
     # Committing changes and closing the connection to the database file
     conn.commit()
     conn.close()
 
+
+
+
 build_sqliteDB(sqlite_db, table_name)
 
-def import_clean_hdf5():
-    pass
-def insert_into_sqlite():
-    # import sqlite3
-    #
-    # persons = [
-    #     ("Hugo", "Boss"),
-    #     ("Calvin", "Klein")
-    # ]
-    #
-    # con = sqlite3.connect(":memory:")
-    #
-    # # Create the table
-    # con.execute("create table person(firstname, lastname)")
-    #
-    # # Fill the table
-    # con.executemany("insert into person(firstname, lastname) values (?,?)", persons)
-    pass
+
+
+
+
+
+
+def insert_into_sqlite(table_name, df,conn):
+    df.to_sql(table_name,conn, if_exists='replace')
+
+
+def import_clean_hdf5(sqlite_db, table_name):
+
+    for file in hdf5_file_list:
+        conn = sqlite3.connect(sqlite_db)
+        c = conn.cursor()
+        f = h5py.File(file, 'r')
+        columns = ['gtl1_delta_time','gtl1_lat','gtl1_lon','gtl1_h_li','gtl1_x_atc', 'gtl1_atl06_quality_summary',
+        'gtr1_delta_time','gtr1_lat','gtr1_lon','gtr1_h_li','gtr1_x_atc', 'gtr1_atl06_quality_summary',
+        'gtl2_delta_time','gtl2_lat','gtl2_lon','gtl2_h_li','gtl2_x_atc', 'gtl2_atl06_quality_summary',
+        'gtr2_delta_time','gtr2_lat','gtr2_lon','gtr2_h_li','gtr2_x_atc', 'gtr2_atl06_quality_summary',
+        'gtl3_delta_time','gtl3_lat','gtl3_lon','gtl3_h_li','gtl3_x_atc', 'gtl3_atl06_quality_summary',
+        'gtr3_delta_time','gtr3_lat','gtr3_lon','gtr3_h_li','gtr3_x_atc', 'gtr3_atl06_quality_summary']
+        df = pd.DataFrame(columns = columns)
+        #gtl1
+        df['gtl1_delta_time'] = f['gt1l']['land_ice_segments']['delta_time']
+        df['gtl1_lat'] = f['gt1l']['land_ice_segments']['latitude']
+        df['gtl1_lon'] = f['gt1l']['land_ice_segments']['longitude']
+        df['gtl1_h_li'] = f['gt1l']['land_ice_segments']['h_li']
+        df['gtl1_x_atc'] = f['gt1l']['land_ice_segments']['ground_track']['x_atc']
+        df['gtl1_atl06_quality_summary'] = f['gt1l']['land_ice_segments']['atl06_quality_summary']
+        #gtr1
+        df['gtr1_delta_time'] = f['gt1l']['land_ice_segments']['delta_time']
+        df['gtr1_lat'] = f['gt1l']['land_ice_segments']['latitude']
+        df['gtr1_lon'] = f['gt1l']['land_ice_segments']['longitude']
+        df['gtr1_h_li'] = f['gt1l']['land_ice_segments']['h_li']
+        df['gtr1_x_atc'] = f['gt1l']['land_ice_segments']['ground_track']['x_atc']
+        df['gtr1_atl06_quality_summary'] = f['gt1l']['land_ice_segments']['atl06_quality_summary']
+        #gtl2
+        df['gtl2_delta_time'] = f['gt1l']['land_ice_segments']['delta_time']
+        df['gtl2_lat'] = f['gt1l']['land_ice_segments']['latitude']
+        df['gtl2_lon'] = f['gt1l']['land_ice_segments']['longitude']
+        df['gtl2_h_li'] = f['gt1l']['land_ice_segments']['h_li']
+        df['gtl2_x_atc'] = f['gt1l']['land_ice_segments']['ground_track']['x_atc']
+        df['gtl2_atl06_quality_summary'] = f['gt1l']['land_ice_segments']['atl06_quality_summary']
+        #gtr2
+        df['gtr2_delta_time'] = f['gt1l']['land_ice_segments']['delta_time']
+        df['gtr2_lat'] = f['gt1l']['land_ice_segments']['latitude']
+        df['gtr2_lon'] = f['gt1l']['land_ice_segments']['longitude']
+        df['gtr2_h_li'] = f['gt1l']['land_ice_segments']['h_li']
+        df['gtr2_x_atc'] = f['gt1l']['land_ice_segments']['ground_track']['x_atc']
+        df['gtr2_atl06_quality_summary'] = f['gt1l']['land_ice_segments']['atl06_quality_summary']
+        #gtl3
+        df['gtl3_delta_time'] = f['gt1l']['land_ice_segments']['delta_time']
+        df['gtl3_lat'] = f['gt1l']['land_ice_segments']['latitude']
+        df['gtl3_lon'] = f['gt1l']['land_ice_segments']['longitude']
+        df['gtl3_h_li'] = f['gt1l']['land_ice_segments']['h_li']
+        df['gtl3_x_atc'] = f['gt1l']['land_ice_segments']['ground_track']['x_atc']
+        df['gtl3_atl06_quality_summary'] = f['gt1l']['land_ice_segments']['atl06_quality_summary']
+        #gtr3
+        df['gtr3_delta_time'] = f['gt1l']['land_ice_segments']['delta_time']
+        df['gtr3_lat'] = f['gt1l']['land_ice_segments']['latitude']
+        df['gtr3_lon'] = f['gt1l']['land_ice_segments']['longitude']
+        df['gtr3_h_li'] = f['gt1l']['land_ice_segments']['h_li']
+        df['gtr3_x_atc'] = f['gt1l']['land_ice_segments']['ground_track']['x_atc']
+        df['gtr3_atl06_quality_summary'] = f['gt1l']['land_ice_segments']['atl06_quality_summary']
+
+        insert_into_sqlite(table_name, df, conn)
+        conn.commit()
+        conn.close()
+        break
+
+
+import_clean_hdf5(sqlite_db, table_name)
+
+
+
 def index_sqlite():
     pass
 
 
-#
+
+
+
+
+
 #
 sqlite_file = 'gt1l_test.sqlite'    # name of the sqlite database file
 # table_name = 'my_table_1'  # name of the table to be created
